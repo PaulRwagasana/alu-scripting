71cce3b45d61ef87b961c1dfe8b1e75dd751f0ae
PaulRwagasana <p.rwagasana@alustudent.com>
2024-10-26 11:30:08 +0000

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,17 @@
 #!/usr/bin/python3
-""" top_ten.py """
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit."""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)  # Print None for non-existent subreddit
-        return
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        posts = response.json().get('data', {}).get('children', [])
-        if not posts:
-            print(None)
-            return
-        for post in posts[:10]:  # Limit to first 10 posts
-            title = post['data'].get('title')
-            if title:  # Only print if title exists
-                print(title)
-    except (ValueError, KeyError, TypeError):
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
