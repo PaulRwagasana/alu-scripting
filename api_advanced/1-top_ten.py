a682c52386fa406044a30de11c4d4d8a727a8808
PaulRwagasana <p.rwagasana@alustudent.com>
2024-10-26 11:12:35 +0000

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -5,30 +5,13 @@ import requests
 
 
 def top_ten(subreddit):
-    """Main function to print the top 10 hot posts of a subreddit"""
-    URL = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
     HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
-
     try:
-        response = requests.get(URL, headers=HEADERS, allow_redirects=False)
-        # Check if the request was successful (HTTP status code 200)
-        if response.status_code == 200:
-            hot_posts = response.json().get("data", {}).get("children", [])
-            if hot_posts:
-                # Print the title of each hot post
-                for post in hot_posts:
-                    print(post.get('data', {}).get('title', 'No title'))
-            else:
-                print(None)
-        else:
-            # If the subreddit does not exist or the request fails
-            print(None)
-
-    except requests.RequestException:
-        # Catch network-related errors
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
-
-    except ValueError:
-        # Catch JSON decoding errors
-        print(None)
-
