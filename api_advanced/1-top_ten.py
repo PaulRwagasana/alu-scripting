a35f43c67751ac2ce01a73a5f2682847be09f28a
PaulRwagasana <p.rwagasana@alustudent.com>
2024-10-26 21:16:17 +0000

    Task

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+""" top_ten.py """
 import requests
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
-
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
+    """Prints the titles of the first 10 hot posts listed in a subreddit."""
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code != 200:
+        print(None)  # Print None for non-existent subreddit
+        return
+    try:
+        posts = response.json().get('data', {}).get('children', [])
+        if not posts:
+            print(None)
+            return
+        for post in posts[:10]:  # Limit to first 10 posts
+            title = post['data'].get('title')
+            if title:  # Only print if title exists
+                print(title)
+    except (ValueError, KeyError, TypeError):
         print(None)
