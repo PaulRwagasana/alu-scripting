9f3822779ef189a375d6bb84c2bc55919249e797
PaulRwagasana <p.rwagasana@alustudent.com>
2024-12-03 13:18:42 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-""" top_ten.py """
+""""Doc"""
 import requests
 
 
 def top_ten(subreddit):
-    """Prints the titles of the first 10 hot posts listed in a subreddit."""
-    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)  # Print None for non-existent subreddit
-        return
-    try:
-        posts = response.json().get('data', {}).get('children', [])
-        if not posts:
-            print(None)
-            return
-        for post in posts[:10]:  # Limit to first 10 posts
-            title = post['data'].get('title')
-            if title:  # Only print if title exists
-                print(title)
-    except (ValueError, KeyError, TypeError):
+    """"Doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10" \
+        .format(subreddit)
+
+    res = requests.get(url,
+                       headers={
+                           'User-Agent': 'Mozilla/5.0'})
+
+    if res.status_code != 200:
         print(None)
+    else:
+        json_response = res.json()
+        posts = json_response.get('data').get('children')
+        [print(post.get('data').get('title')) for post in posts]
