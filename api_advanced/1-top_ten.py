d5ae675b66dc908f8e5a4ab14a9eb870c386f472
PaulRwagasana <p.rwagasana@alustudent.com>
2024-12-03 13:10:49 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,26 +1,17 @@
 #!/usr/bin/python3
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-        
-        # Check if the subreddit exists or if there is a redirect
-        if response.status_code == 200:
-            data = response.json()
-            posts = data.get('data', {}).get('children', [])
-            
-            if not posts:
-                print(None)
-            else:
-                for post in posts:
-                    print(post['data']['title'])
-        else:
-            print(None)
-
-    except requests.exceptions.RequestException as e:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
