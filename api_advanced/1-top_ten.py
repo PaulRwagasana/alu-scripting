34a03cb1479090c8f3d80425ec0c6fdd8b9da382
PaulRwagasana <p.rwagasana@alustudent.com>
2024-12-03 13:07:43 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,25 @@
-#!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+import requests
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+def top_ten(subreddit):
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    params = {'limit': 10}
+
+    try:
+        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        
+        # Check if the subreddit exists or if there is a redirect
+        if response.status_code == 200:
+            data = response.json()
+            posts = data.get('data', {}).get('children', [])
+            
+            if not posts:
+                print(None)
+            else:
+                for post in posts:
+                    print(post['data']['title'])
+        else:
+            print(None)
+
+    except requests.exceptions.RequestException as e:
+        print(None)
